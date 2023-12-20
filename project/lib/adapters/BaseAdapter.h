73006ef5de4236a47dc731e681e0c7898f980efd
31steets31 <stepaha78@gmail.com>
2023-12-20 18:24:23 +0300

    course passed!!!

--- project/lib/adapters/BaseAdapter.h
+++
@@ -1,9 +0,0 @@
-#pragma once 
-
-class BaseAdapter
-{
-public:
-    virtual ~BaseAdapter() = default;
-
-    virtual void request() = 0;
-};
--- project/lib/objects/visibleobject/BaseModel.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <VisibleObject.h>
-
-class BaseModel: public VisibleObject
-{
-    virtual bool isVisible() const override;
-    virtual bool isComposite() const override;
-};
